9f5d8605ad6ceb769deeb31fa7a211dfa9246959
JennyHolberton <5704@holbertonstudents.com>
2023-05-16 23:23:47 +1000

    Updated setup and yml file

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+apt-get update && apt-get install -y hugo make
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
-apt-get update && apt-get install -y hugo make
-make build
+rm hugo_extended_0.84.0_Linux-64bit.tar.gz
