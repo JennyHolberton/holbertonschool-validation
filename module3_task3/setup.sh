be263dee30b86f4f66ede2a7389c3d4c352668d4
Jenny <5704@holbertonstudents.com>
2023-05-22 14:44:52 +1000

    removed zip installation

--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -4,6 +4,4 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
 rm hugo_extended_0.84.0_Linux-64bit.tar.gz
-sudo apt install zip
-sudo apt install unzip
 sudo npm install -g markdownlint-cli
